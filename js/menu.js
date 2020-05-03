cce8ca99eb4607e884114e73ead0030ddc412b6d
kerik <kerik@Eriks-Pro.local>
2020-05-03 13:20:48 +0200

    opravy

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-// function togglemenu() {
-//   const elem = document.getElementById('menu');
-
-//   if (elem.style.display === 'none' || elem.style.display === '') {
-//     elem.style.display = 'flex';
-//   } else {
-//     elem.style.display = '';
-//   }
-// }
